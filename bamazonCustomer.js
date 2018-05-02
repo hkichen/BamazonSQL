50ac53fd81793d5ef93a16336ab5f1f3584fbc52
hkichen <hkichen@gmail.com>
2018-05-02 19:19:17 -0400

    pushing initial project

---
+++ bamazonCustomer.js
@@ -0,0 +1,46 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "pass",
+    database: "bamazonDB"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected to Database!")
+    //run function that welcome you to store and lists all products
+})
+
+function openStore(){
+    console.log(
+        "\n--------------------"
+        + "Welcome to Bamazon, where some things can be found~!"
+        + "\n--------------------"
+        + "\n\nThis is what we currently have in stock: "
+    );
+
+    connection.query("select * from products", function (err, resp) {
+        if (err) throw err;
+        for (var i = 0; i < resp.length; i++) {
+            console.log(
+                "ID: " + resp[i].id 
+                + " || Item Name: " + resp[i].product_name
+                + " || Price: " + resp[i].price
+            )
+        }
+        runPrompt();
+    })
+}
+
+// function runPrompt() {
+//     inquirer.prompt({
+//         name: "buy",
+//         message: "What Would you like to buy? (Please indicate by ID)"
+//     }).then(function(answer) {
+
+//     }
+// }
---
+++ bamazonDB.sql
@@ -0,0 +1,39 @@
+drop database if exists bamazondb;
+create database bamazondb;
+
+use bamazondb;
+drop table if exists products;
+
+create table products (
+	item_id int auto_increment not null,
+  product_name varchar(240),
+  department_name varchar(240),
+  price decimal not null,
+  stock_quantity int not null,
+  PRIMARY KEY (item_id)
+);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values ("earbuds", "electronics", 14.98, 20);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values ("mug", "kitchen", 10.50, 20);
+
+
+insert into products (product_name, department_name, price, stock_quantity)
+values ("Hooded Sweatshirt", "Clothes", 21.34, 20);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values ("Azalea Ahrub - 3g", "Garden", 24.30, 15);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values ("organic Honey - 16oz", "Food", 44.50, 10);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values ("Stone Ground Crackers", "Food", 6.50, 20);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values ("Lace Curtains", "Home", 14.98, 20);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values ("Slinky", "Toys", 14.98, 20);
