7ba8adc8518cbe7bcf9d6e929ae27419362b6070
hkichen <hkichen@gmail.com>
2018-05-02 21:34:34 -0400

    testing new table format

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var table = require("cli-table");
+var Table = require("cli-table");
 //no more struggling to format the table (yayyyy)
 
 var connection = mysql.createConnection({
@@ -27,41 +27,48 @@ function openStore(){
     
     connection.query("select * from products", function (err, resp) {
         if (err) throw err;
+        
+        //make a display table
+        var showTable = new table({
+            head: ["Item ID", "Product Name", "Department", "Price", "Stock Quantity"],
+            colWidths: [10, 45, 20, 10, 10]
+        });
+
         for (var i = 0; i < resp.length; i++) {
-            console.log(
-                "ID: " + resp[i].item_id 
-                + " || Item Name: " + resp[i].product_name + "  "
-                + " || Price: " + resp[i].price + " "
-            )
+
+            showTable.push([
+                resp[i].item_id, resp[i].product_name, resp[i].department_name, resp[i].price, resp[i].stock_quantity
+            ])
         }
+        console.log(showTable.toString());
         runPrompt();
     })
 }
 
-function runPrompt() {
-    connection.query("select * from products", function (err, resp) {
-        if (err) throw err;
+// function runPrompt() {
+//     connection.query("select * from products", function (err, resp) {
+//         if (err) throw err;
 
-        inquirer.prompt({
-            name: "buy",
-            message: "What Would you like to buy? (Please indicate by ID)",
-        }).then(function(answer) {
-            if(answer.buy === resp.item_id) {
-                inquirer.prompt([
-                    {
-                        name: "howMany",
-                        message: "How many would you like to purchase?",
-                        validate: function(value) {
-                            if (isNaN(value) === false) {
-                                return true;
-                            }
-                            return false;
-                        }
-                    }
-                ]).then(function(answer) {
-                    if (answer.howMany === )
-                })
-            }
-        }
-    });
-}
+//         inquirer.prompt({
+//             name: "buy",
+//             message: "What Would you like to buy? (Please indicate by ID)",
+//         }).then(function(answer) {
+//             if(answer.buy === resp.item_id) {
+//                 inquirer.prompt([
+//                     {
+//                         name: "howMany",
+//                         message: "How many would you like to purchase?",
+//                         validate: function(value) {
+//                             if (isNaN(value) === false) {
+//                                 return true;
+//                             }
+//                             return false;
+//                         }
+//                     }
+//                 ]).then(function(answer) {
+//                     if (answer.howMany === )
+//                 })
+//             }
+//         }
+//     });
+// }
